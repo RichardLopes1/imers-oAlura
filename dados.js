cbbb36264e73cee6d057aa3e5e6d357a446c5a28
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-06 09:42:01 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,11 @@
+let dados = [
+    "Bruno Mars",
+    "Adele",
+    "Rihanna"
+];
+
+let Bruno = {
+    titulo: "Bruno Mars",
+    descricao: "Bruno Mars, cujo nome de batismo é Peter Gene Hernandez, é um talentosoartista americano, nascido e criado no Havaí. Conhecido por sua voz soul envolvente, coreografias vibrantes e letras cativantes,Bruno Mars conquistou o mundo da música pop com hits como Just the Way You Are, Uptown Funk e Locked Out of Heaven.",
+    link: "https://pt.wikipedia.org/wiki/Bruno_Mars"
+}
